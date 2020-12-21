b8e7efbc10028c593822df2bea3640159c2696e1
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-21 12:10:56 +0300

    added comparison

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,6 @@
 # Binary Search vs Naive Search
+import random
+import time
 
 def naive_search(l, target):
     for i in range(len(l)):
@@ -16,11 +18,38 @@ def binary_search(l, target, low=None, high=None):
     
     if high < low:
         return -1
-        
+
     midpoint = (low + high) // 2
     if l[midpoint] == target:
         return midpoint
     elif target < l[midpoint]:
-        return binary_search(l, target, low, high=midpoint-1)
+        return binary_search(l, target, low, midpoint-1)
     else:
-        return binary_search(l, target, low=midpoint+1, high)
+        return binary_search(l, target, midpoint+1, high)
+
+
+if __name__ == '__main__':
+    """
+    l = [1, 3, 5, 10, 12]
+    target = 10
+    print(naive_search(l, target))
+    print(binary_search(l, target))
+    """
+    length = 10000
+    sorted_list = set()
+
+    while len(sorted_list) < length:
+        sorted_list.add(random.randint(-3*length, 3*length))
+    sorted_list = sorted(list(sorted_list))
+
+    start = time.time()
+    for target in sorted_list:
+        naive_search(sorted_list, target)
+    end = time.time()
+    print(f"Naive Search time: {(end - start)/length} seconds")
+
+    start = time.time()
+    for target in sorted_list:
+        binary_search(sorted_list, target)
+    end = time.time()
+    print(f"Binary Search time: {(end - start)/length} seconds")
