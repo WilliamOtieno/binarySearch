9ec5048b4280bfd3020e03d8ee18f23546ddc7cb
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-21 11:58:39 +0300

    added binary func

---
+++ binary_search.py
@@ -0,0 +1,26 @@
+# Binary Search vs Naive Search
+
+def naive_search(l, target):
+    for i in range(len(l)):
+        if l[i] == target:
+            return i
+    
+    return -1
+
+
+def binary_search(l, target, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(l) -1
+    
+    if high < low:
+        return -1
+        
+    midpoint = (low + high) // 2
+    if l[midpoint] == target:
+        return midpoint
+    elif target < l[midpoint]:
+        return binary_search(l, target, low, high=midpoint-1)
+    else:
+        return binary_search(l, target, low=midpoint+1, high)
